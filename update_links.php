467a6d0887698534641c617a53afb2b1fd442c6b
mmjavadgh <102357128+mmjavadgh@users.noreply.github.com>
2023-11-24 07:53:35 +0330

    Create update_links.php

---
+++ update_links.php
@@ -0,0 +1,24 @@
+<?php
+
+$telegramBotToken = '6520725138:AAF9YpmlJ0ypzFyZwaNr0hf_60xh9RW_kFc';
+$channelId = '@TVCminer'; // یا شناسه کانال مثل '-1001234567890'
+
+// Fetch the messages from the Telegram channel
+$apiUrl = "https://api.telegram.org/bot{$telegramBotToken}/getChatHistory?chat_id={$channelId}&limit=10";
+$response = file_get_contents($apiUrl);
+$data = json_decode($response, true);
+
+// Extract V2Ray links from messages and append to links.txt
+$links = [];
+foreach ($data['result']['messages'] as $message) {
+    if (isset($message['text'])) {
+        preg_match_all('/vmess:\/\/[^\s]+/', $message['text'], $matches);
+        $links = array_merge($links, $matches[0]);
+    }
+}
+
+if (!empty($links)) {
+    file_put_contents('links.txt', implode("\n", $links));
+}
+
+?>
