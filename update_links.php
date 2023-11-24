862b9b177636705120d480d5cfea869006fd284c
mmjavadgh <102357128+mmjavadgh@users.noreply.github.com>
2023-11-24 07:58:07 +0330

    Update update_links.php

--- update_links.php
+++ update_links.php
@@ -7,7 +7,7 @@ $channelId = '@TVCminer'; // یا شناسه کانال مثل '-1001234567890'
 $apiUrl = "https://api.telegram.org/bot{$telegramBotToken}/getChatHistory?chat_id={$channelId}&limit=10";
 $response = file_get_contents($apiUrl);
 $data = json_decode($response, true);
-
+ 
 // Extract V2Ray links from messages and append to links.txt
 $links = [];
 foreach ($data['result']['messages'] as $message) {
